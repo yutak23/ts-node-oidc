21be43e9f962e1e630b04bb08d993d43a69e3e1c
yutak23 <yutapilafu@gmail.com>
2023-12-15 17:12:25 +0900

    chore: typescriptの設定を変更

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,27 +1,26 @@
 /* eslint-env node */
 module.exports = {
 	root: true,
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: './tsconfig.eslint.json',
-		tsconfigRootDir: __dirname
-	},
-	plugins: ['@typescript-eslint'],
-	extends: [
-		'eslint:recommended',
-		'airbnb-base',
-		'airbnb-typescript/base',
-		'plugin:@typescript-eslint/recommended',
-		'prettier'
-	],
-	overrides: [
-		{
-			extends: ['plugin:@typescript-eslint/recommended-requiring-type-checking'],
-			files: ['./**/*.{ts,tsx}']
-		}
-	],
+	extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
 	rules: {
 		'import/no-extraneous-dependencies': ['warn', { packageDir: './' }]
 	},
+	overrides: [
+		{
+			files: ['./**/*.{ts,tsx,cts}'],
+			extends: [
+				'plugin:@typescript-eslint/recommended',
+				'airbnb-typescript/base',
+				'plugin:@typescript-eslint/recommended-requiring-type-checking',
+				'prettier'
+			],
+			parser: '@typescript-eslint/parser',
+			parserOptions: { project: './tsconfig.eslint.json' },
+			plugins: ['@typescript-eslint'],
+			rules: {
+				'import/no-extraneous-dependencies': ['warn', { packageDir: './' }]
+			}
+		}
+	],
 	ignorePatterns: ['build', 'node_modules']
 };
